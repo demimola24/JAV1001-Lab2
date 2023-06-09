26a60c67d7d0ea391ba8497e482137852a94e730
demimola24 <trideks0331>
2023-06-09 10:32:20 -0400

    final testing and code formating

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,17 +1,27 @@
+import java.util.Scanner
+import java.util.*
 
 
-fun casearCipher(value: String, shiftValue: Int): String{
+fun main() {
+    arrayReverseTestCases()
+    arrayContainsTestCases()
+    arrayAvgTestCases()
+    //casearCipherTesting()
+}
+
+
+fun casearCipher(value: String, shiftValue: Int): String {
     var encryptedValue = ""
     val len: Int = value.length
     for (x in 0 until len) {
-        encryptedValue += if ((value[x] + shiftValue) > 'z') (value[x] - (26 - shiftValue)) as Char else (value.get(
+        encryptedValue += if ((value[x] + shiftValue) > 'z') (value[x] - (26 - shiftValue)) else (value.get(
             x
         ) + shiftValue)
     }
     return encryptedValue
 }
 
-fun casearCipherTesting(){
+fun casearCipherTesting() {
     // Getting input from the user
     val input = Scanner(System.`in`)
 
@@ -21,10 +31,10 @@ fun casearCipherTesting(){
     println("Enter a shift value (Int): ")
     val shiftInput = input.nextInt()
 
-    println("Encrypted value is : ${casearCipher(numberInput,shiftInput)}")
+    println("Encrypted value is : ${casearCipher(numberInput, shiftInput)}")
 }
 
-fun arrayAveraging(input: Array<Int>): Double{
+fun arrayAveraging(input: Array<Int>): Double {
     return if (input.isNullOrEmpty()) 0.0
     else {
         var sum = 0.0
@@ -34,30 +44,32 @@ fun arrayAveraging(input: Array<Int>): Double{
         sum / input.size
     }
 }
-fun arrayAvgTestCases(){
-    println("Average is ${arrayAveraging(arrayOf(3,2,4))}")
-    println("Average is ${arrayAveraging(arrayOf(-2,2,-4))}")
-    println("Average is ${arrayAveraging(arrayOf(0,0,0))}")
+
+fun arrayAvgTestCases() {
+    println("Average is ${arrayAveraging(arrayOf(3, 2, 4))}")
+    println("Average is ${arrayAveraging(arrayOf(-2, 2, -4))}")
+    println("Average is ${arrayAveraging(arrayOf(0, 0, 0))}")
 
 }
 
-fun arrayContains(values: Array<Int>, query: Int): Boolean{
-    for(number in values){
-        if(number==query){
+fun arrayContains(values: Array<Int>, query: Int): Boolean {
+    for (number in values) {
+        if (number == query) {
             return true
         }
     }
     return false
 }
-fun arrayContainsTestCases(){
-    println(arrayContains(arrayOf(3,2,4),2))
-    println(arrayContains(arrayOf(-3,-2,-4),2))
-    println(arrayContains(arrayOf(0,0,0),2))
+
+fun arrayContainsTestCases() {
+    println(arrayContains(arrayOf(3, 2, 4), 2))
+    println(arrayContains(arrayOf(-3, -2, -4), 2))
+    println(arrayContains(arrayOf(0, 0, 0), 2))
 
 }
 
 
-fun arrayReverse(values: Array<Int>): Array<Int>{
+fun arrayReverse(values: Array<Int>): Array<Int> {
     val mutableList = mutableListOf<Int>()
     for (i in values.lastIndex downTo 0) {
         mutableList.add(values[i])
@@ -65,9 +77,9 @@ fun arrayReverse(values: Array<Int>): Array<Int>{
     return mutableList.toTypedArray()
 }
 
-fun arrayReverseTestCases(){
-    println(Arrays.toString(arrayReverse(arrayOf(-3,-2,-4))))
-    println(Arrays.toString(arrayReverse(arrayOf(1,1,3))))
-    println(Arrays.toString(arrayReverse(arrayOf(-3,-0,100))))
+fun arrayReverseTestCases() {
+    println(Arrays.toString(arrayReverse(arrayOf(-3, -2, -4))))
+    println(Arrays.toString(arrayReverse(arrayOf(1, 1, 3))))
+    println(Arrays.toString(arrayReverse(arrayOf(-3, -0, 100))))
 }
 
